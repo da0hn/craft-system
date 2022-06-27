122cb5500f5ee3f953effce77cadf08e0f105fbc
Da0hn <gabrielhondacba@gmail.com>
2022-06-27 08:49:53 -0400

    refactor: extract `name` validation to static method

--- recipe/src/main/java/org/da0hn/recipe/core/domain/Recipe.java
+++ recipe/src/main/java/org/da0hn/recipe/core/domain/Recipe.java
@@ -12,15 +12,16 @@ public class Recipe {
     final String name
   ) {
     this.id = Objects.requireNonNull(id, "Recipe id must be not null");
+    ifNameIsEmptyThrowException(name);
     this.name = Objects.requireNonNull(name, "Recipe name must be not null");
-
-    if(name.isEmpty() || name.isBlank()) {
-      throw new RecipeValidationException("Recipe name must be not empty");
-    }
-
     this.constraints = ConstraintContainer.empty();
   }
 
+  private static void ifNameIsEmptyThrowException(final String name) {
+    if(name.isEmpty() || name.isBlank()) {
+      throw new RecipeValidationException("Recipe name must be not empty");
+    }
+  }
 
   public boolean hasConstraints() {
     return !this.constraints.isEmpty();
