b1276bed4fa0bfb46487c876d23d3f8787cca6b3
Da0hn <gabrielhondacba@gmail.com>
2022-07-03 10:09:48 -0400

    chore: add utility class to constants messages

---
+++ recipe/src/main/java/org/da0hn/recipe/core/shared/RecipeMessages.java
@@ -0,0 +1,21 @@
+package org.da0hn.recipe.core.shared;
+
+public final class RecipeMessages {
+
+  public static final String ITEM_TYPE_NOT_NULL = "item.type.not-null";
+
+  public static final String ITEM_ID_NOT_NULL = "item.id.not-null";
+
+  public static final String ITEM_NAME_NOT_NULL = "item.name.not-null";
+
+  public static final String ITEM_NAME_NOT_EMPTY = "item.name.not-empty";
+
+  public static final String ITEM_QUANTITY_NOT_NULL = "item.quantity.not-null";
+
+  public static final String ITEM_QUANTITY_NEGATIVE = "item.quantity.not-negative";
+
+  private RecipeMessages() {
+  }
+
+
+}
