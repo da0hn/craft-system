803c821f742c2331ad8b6ec1a6e25064edca1c40
Da0hn <gabrielhondacba@gmail.com>
2022-07-04 07:57:29 -0400

    chore: add `@FunctionalInterface` annotation

--- commons/src/main/java/org/da0hn/commons/core/usecases/UseCase.java
+++ commons/src/main/java/org/da0hn/commons/core/usecases/UseCase.java
@@ -1,8 +1,9 @@
 package org.da0hn.commons.core.usecases;
 
+@FunctionalInterface
 public interface UseCase<INPUT extends UseCase.Input, OUTPUT extends UseCase.Output> {
 
-  OUTPUT execute(INPUT input);
+  OUTPUT execute(final INPUT input);
 
   interface Input {}
 
