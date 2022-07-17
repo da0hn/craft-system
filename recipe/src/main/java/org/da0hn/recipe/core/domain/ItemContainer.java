497c6e4b10969b9ce49e4c515414d030ed881dec
Da0hn <gabrielhondacba@gmail.com>
2022-07-17 10:47:12 -0400

    feat: add static constructors for `Items` implementation

--- recipe/src/main/java/org/da0hn/recipe/core/domain/ItemContainer.java
+++ recipe/src/main/java/org/da0hn/recipe/core/domain/ItemContainer.java
@@ -2,29 +2,48 @@ package org.da0hn.recipe.core.domain;
 
 import org.da0hn.recipe.core.shared.RecipeMessages;
 
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Stream;
 
 
-public class ItemContainer implements Items {
+public final class ItemContainer implements Items {
 
-  private final Set<ItemModel> items = new HashSet<>();
+  private final Collection<ItemModel> items = new LinkedHashSet<>();
+
+  private ItemContainer() {
+  }
+
+  private ItemContainer(final Collection<? extends ItemModel> items) {
+    this.items.addAll(items);
+  }
 
   public static Items empty() {
     return new ItemContainer();
   }
 
-  @Override public boolean isEmpty() {
+  public static Items of(final Collection<? extends ItemModel> items) {
+    return new ItemContainer(items);
+  }
+
+  public static Items of(final ItemModel... items) {
+    return new ItemContainer(List.of(items));
+  }
+
+  @Override
+  public boolean isEmpty() {
     return this.items.isEmpty();
   }
 
-  @Override public void add(final ItemModel item) {
+  @Override
+  public void add(final ItemModel item) {
     this.items.add(Objects.requireNonNull(item, RecipeMessages.ITEM_NOT_NULL));
   }
 
-  @Override public Stream<ItemModel> stream() {
+  @Override
+  public Stream<ItemModel> stream() {
     return this.items.stream();
   }
 
