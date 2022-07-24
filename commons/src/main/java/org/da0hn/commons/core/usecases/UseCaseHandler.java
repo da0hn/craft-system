43cb48b675db3ac17003ad54cc390654d68ffb46
Gabriel Honda <gabriel.honda@totemti.com.br>
2022-07-24 09:13:57 -0400

    fix: add FunctionInterface annotation

--- commons/src/main/java/org/da0hn/commons/core/usecases/UseCaseHandler.java
+++ commons/src/main/java/org/da0hn/commons/core/usecases/UseCaseHandler.java
@@ -3,6 +3,7 @@ package org.da0hn.commons.core.usecases;
 import java.util.function.Function;
 
 
+@FunctionalInterface
 public interface UseCaseHandler {
 
   <RESULT, INPUT extends UseCase.Input, OUTPUT extends UseCase.Output> RESULT handle(
--- recipe/src/main/java/org/da0hn/recipe/core/ports/RecipeRepository.java
+++ recipe/src/main/java/org/da0hn/recipe/core/ports/RecipeRepository.java
@@ -3,6 +3,7 @@ package org.da0hn.recipe.core.ports;
 import org.da0hn.recipe.core.domain.RecipeModel;
 
 
+@FunctionalInterface
 public interface RecipeRepository {
 
   void create(RecipeModel recipe);
